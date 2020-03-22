b1a116864f12ba7a4d6d1c43288e1aa2e0ab27ef
hakyoung0926 <43024383+hakyoung0926@users.noreply.github.com>
2020-03-22 17:20:18 +0900

    200322 GUI for Crawling

--- CrawlGui.py
+++ CrawlGui.py
@@ -10,10 +10,16 @@ import time
 import io
 path = "C:/dev/chromedriver.exe"
 search = None
+selectNum = None
+status = False
 class Worker(QThread):
     finished = pyqtSignal(list)
     def run(self):
+        global status
         global search
+        global selectNum
+        global path
+        status = True
         driver = webdriver.Chrome(path)
         driver.implicitly_wait(3)
         driver.get('http://www.kyobobook.co.kr/index.laf')
@@ -41,12 +47,74 @@ class Worker(QThread):
                 except NoSuchElementException:
                     break
             self.finished.emit(books)
+            while True :
+                if selectNum == None:
+                    pass
+                else : break
+            selectedBook = driver.find_element_by_xpath("//*[@id='search_list']/tr[" + str(selectNum) + "]/td[2]/div[2]/a")
+            selectedBookName = driver.find_element_by_xpath("//*[@id='search_list']/tr[" + str(selectNum) + "]/td[2]/div[2]/a/strong").text
         except NoSuchElementException:
-            print("검색하시려는 책이 없습니다. 종료합니다")
             self.finished.emit(books)
             driver.quit()
-            exit()
 
+        selectedBook.click()
+        window_before = driver.window_handles[0]
+        try:
+            showReview = driver.find_element_by_link_text("전체보기")
+            print("검색하신 책의 리뷰를 찾았습니다.")
+        except NoSuchElementException:
+            print("검색하신 책의 리뷰가 없습니다. 종료합니다")
+            driver.quit()
+        showReview.click()
+        window_after = driver.window_handles[1]
+        driver.switch_to.window(window_after)
+
+        print("현재윈도우 URL : ", driver.current_url)
+        print("변경전윈도우: ", str(window_before))
+        print("변경후윈도우: ", str(window_after))
+
+        html = driver.page_source
+        bsObject = BeautifulSoup(html, "html.parser")
+        reviews = bsObject.find("ul", {"class": "list_detail_booklog"})
+        pages = bsObject.find("div", {"class": "list_paging"}).find("ul").find_all('li')
+        reviewNum = 1
+        for i in range(1, len(pages) + 1):
+            html = driver.page_source
+            bsObject = BeautifulSoup(html, "html.parser")
+            reviews = bsObject.find("ul", {"class": "list_detail_booklog"})
+            reviewslis = reviews.find_all('li')
+            for review in reviewslis:
+                reviewC = review.find("div", {"class": "content"})
+                if reviewC is None:
+                    continue
+                # savedDir = "./reviewF/"
+                Path("./" + selectedBookName).mkdir(parents=True, exist_ok=True)
+                savedDir = "./" + selectedBookName + "/"
+                savedName = str(reviewNum) + ".txt"
+                print(type(reviewC))
+                reviewContent = reviewC.get_text()
+                # 데이터 띄어쓰기,줄바꿈 제거
+                reviewContent = " ".join(reviewContent.split())
+                reviewContent = reviewContent.replace('>', '').replace('<', '').replace('*', '').replace('-',
+                                                                                                         '').replace(
+                    '#', '').replace('^', '').replace('~', '')
+                # reviewContent = reviewContent.splitlines()
+                path = savedDir + savedName
+                with open(path, "w", encoding="cp949", errors='ignore') as f:
+                    f.write(reviewContent)
+                    print(reviewNum, "번째 리뷰가 저장되었습니다.")
+                f.close()
+                reviewNum = reviewNum + 1
+            if i < len(pages):
+                driver.find_element_by_link_text(str(i + 1)).click()
+            else:
+                f.close()
+                # driver.close()
+                # driver.switch_to.window(window_before)
+                # driver.close()
+                driver.quit()
+        print("리뷰 추출이 완료되었습니다.")
+        status = False
 class Form(QtWidgets.QDialog):
     def __init__(self,parent=None):
         super().__init__()
@@ -54,18 +122,31 @@ class Form(QtWidgets.QDialog):
         self.ui.show()
         self.ui.search_btn.clicked.connect(self.searchBook)
         self.ui.quit_btn.clicked.connect(self.quitGUI)
+        self.ui.bookList.itemDoubleClicked.connect(self.select_book)
     def quitGUI(self):
+        QtWidgets.QMessageBox.about(self, 'Message', '프로그램을 종료합니다')
         exit()
     def searchBook(self):
         global search
         search = self.ui.search_name.text()
         self.worker = Worker()
-        self.worker.finished.connect(self.update_number)
+        self.worker.finished.connect(self.update_list)
         self.worker.start()
+    def select_book(self):
+        global selectNum
+        # QtWidgets.QMessageBox.about(self, 'Message', self.ui.bookList.currentItem().text())
+        selectNum = self.ui.bookList.currentRow()+1
+        # print(self.ui.bookList.currentRow())
+        print(self.ui.bookList.currentItem().text())
     @pyqtSlot(list)
-    def update_number(self, data):
-        for i in data:
-            self.ui.bookList.addItem(str(i))
+    def update_list(self, data):
+        self.ui.bookList.clear()
+        print(type(data))
+        if len(data)==0:
+            QtWidgets.QMessageBox.about(self,'Message','검색 결과 없음, 다시검색 ')
+        else:
+            for i in data:
+                self.ui.bookList.addItem(str(i))
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
